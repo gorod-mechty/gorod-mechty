7faf75f39c8ef6e0576bac60877a726eeed3a0cc
Radu Ene <message.raduene@gmail.com>
2016-05-19 17:29:48 +0300

    footer: initial implementation

---
+++ blocks/contacts/contacts.bemtree.js
@@ -0,0 +1,12 @@
+block('contacts').content()([
+    {
+        elem: 'contact',
+        elemMods: { type: 'phone' },
+        content: '+7 978 859 29 63'
+    },
+    {
+        elem: 'contact',
+        elemMods: { type: 'email' },
+        content: 'info@gorod-mechty.org'
+    }
+]);
