caba940df62ef90a455af11a4261eff2c4f6e1af
Radu Ene <message.raduene@gmail.com>
2017-03-21 10:19:39 +0200

    Added news bloc in page-body

---
+++ blocks/project-money/project-money.bemhtml.js
@@ -0,0 +1,5 @@
+block('project-money')(
+    elem('item').tag()('dl'),
+    elem('item-title').tag()('dt'),
+    elem('item-quantity').tag()('dd')
+);
---
+++ content/projects/square-on-gogol-street/content.bemjson.js
@@ -0,0 +1,143 @@
+[
+    {
+        block: 'project-money',
+        content: [
+            {
+                elem: 'content',
+                content: [
+                    {
+                        elem: 'item',
+                        content: [
+                            {
+                                elem: 'item-title',
+                                content: 'Нужно'
+                            },
+                            {
+                                elem: 'item-quantity',
+                                content: '25 000 p.'
+                            }
+                        ]
+                    },
+                    {
+                        elem: 'item',
+                        content: [
+                            {
+                                elem: 'item-title',
+                                content: 'Собрано'
+                            },
+                            {
+                                elem: 'item-quantity',
+                                content: '10 050 p.'
+                            }
+                        ]
+                    },
+                    {
+                        elem: 'item',
+                        content: [
+                            {
+                                elem: 'item-title',
+                                content: 'Потрачено'
+                            },
+                            {
+                                elem: 'item-quantity',
+                                content: '8 200 p.'
+                            }
+                        ]
+                    },
+                    {
+                        elem: 'item',
+                        content: [
+                            {
+                                block: 'link',
+                                url: '',
+                                mix: { block: 'project-money', elem: 'link' },
+                                content: 'Помочь проекту'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        elem: 'content',
+        content: [
+            {
+                block: 'text',
+                content: [
+                    {
+                        elem: 'p',
+                        content: 'В городе много мест, где старые деревья были спилены, но не были посажены новые. Мы находим такие места, согласовываем высадку с городскими властями и садим молодые деревья.'
+                    },
+                    {
+                        elem: 'p',
+                        content: 'В настоящее время музей ищет способы и финансовую поддержку для устройства на этом месте – рядом с памятником архитектуры и церковью Марии Магдалины уютного сквера.'
+                    },
+                    {
+                        block: 'link',
+                        url: '',
+                        mix: { block: 'text', elem: 'link' },
+                        content: 'Документация проекта'
+                    }
+                ]
+            },
+            {
+                block: 'gallery',
+            },
+            {
+                block: 'news',
+                content: [
+                    {
+                        elem: 'title',
+                        content: 'Новости проекта'
+                    },
+                    {
+                        elem: 'list',
+                        content: [
+                            {
+                                elem: 'date',
+                                content: '16 июня 2015'
+                            },
+                            {
+                                elem: 'description',
+                                content: 'Продолжаем сбор средств для высадки 20 платанов на улице Жуковского между Гоголя и Горького.'
+                            },
+                            {
+                                elem: 'date',
+                                content: '3 июня 2015'
+                            },
+                            {
+                                elem: 'description',
+                                content: 'Часть платанов на улицах Гагарина и Караимской болеют манилиозом, после окончания дождей начнём лечение.'
+                            },
+                            {
+                                elem: 'date',
+                                content: '27 февраля 2015'
+                            },
+                            {
+                                elem: 'description',
+                                content: 'Сломано восемь деревьвев на улице Караимской, три под корень. Провели обрезку.'
+                            },
+                            {
+                                elem: 'date',
+                                content: '24 февраля 2015'
+                            },
+                            {
+                                elem: 'description',
+                                content: 'Сломано одно дерево на улице Гоголя. Осталось примерно 70 см, провели обрезку, есть шансы, что оно будет расти дальше.'
+                            },
+                            {
+                                elem: 'date',
+                                content: '10 ноября 2013'
+                            },
+                            {
+                                elem: 'description',
+                                content: 'Высажены пятилетние саженцы платана на улице Караимской, перекрёсток с улицей Чехова.'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
