5e7cafd8b64414ce375e1f02bd06ca1bf91e415b
Radu Ene <message.raduene@gmail.com>
2017-03-24 14:13:14 +0200

    Page index

--- blocks/page-body/page-body.bemtree.js
+++ blocks/page-body/page-body.bemtree.js
@@ -1,1 +1,44 @@
-block('page-body').content()(node => node.data.page.content);
+block('page-body').content()(node => {
+    const path = require('path');
+    const list = require(path.resolve('content/projects/projects'));
+    const url = node.data.root + node.data.page.url;
+
+    if (node.data.page.url !== '/')
+        return node.data.page.content;
+
+    // return list.map((item, idx) => {
+
+        // if (idx >= 3) return;
+
+        return {
+            block: 'projects-list',
+            // content: [
+            //     {
+            //         elem: 'item',
+            //         url: url + item.url,
+            //         content: [
+            //             {
+            //                 block: 'project-state',
+            //                 mods: item.state.mods,
+            //                 mix: { block: 'projects-list', elem: 'state' },
+            //                 content: item.state.text
+            //             },
+            //             {
+            //                 elem: 'image',
+            //                 path: url,
+            //                 data: item.image
+            //             },
+            //             {
+            //                 elem: 'title',
+            //                 content: item.title
+            //             },
+            //             {
+            //                 elem: 'description',
+            //                 content: item.description
+            //             }
+            //         ]
+            //     }
+            // ]
+        };
+    // });
+});
